847baf1357d6aec9dde21696dee894a2dbce524f
Kirill Mokevnin <mokevnin@gmail.com>
2022-03-28 22:09:15 -0400

    add state
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- main.tf
+++ main.tf
@@ -7,3 +7,23 @@ terraform {
     }
   }
 }
+
+variable "do_token" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "web1" {
+  image  = "ubuntu-18-04-x64"
+  name   = "web-1"
+  region = "ams3"
+  size   = "s-1vcpu-1gb"
+}
+
+resource "digitalocean_droplet" "web2" {
+  image  = "ubuntu-18-04-x64"
+  name   = "web-2"
+  region = "ams3"
+  size   = "s-1vcpu-1gb"
+}
